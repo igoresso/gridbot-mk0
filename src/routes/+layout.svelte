12fda5b9312d04180f05cbd8e4141c4e9ee91faf
Igor Tyshchenko <33253240+igoresso@users.noreply.github.com>
2024-09-04 21:49:04 +1000

    Implemented general layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import '../app.postcss';
+</script>
+
+<header class="mb-4">
+	<h1 class="text-4xl text-center font-bold">🤖 GridBot-MK0</h1>
+</header>
+
+<main class="flex-grow">
+	<slot />
+</main>
+
+<footer class="flex justify-center mt-2">
+	<span>
+		Made with <span class="text-red-500">♥ </span> by
+		<a href="https://github.com/igoresso">@igoresso</a>
+	</span>
+</footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	const rows = 5;
+	const cols = 5;
+
+	let grid = Array(rows * cols).fill(0);
+	grid[12] = 1;
+</script>
+
+<div class="grid grid-cols-5">
+	{#each grid as cell}
+		<div class="flex justify-center items-center w-20 h-20 bg-base-200 border border-base-300">
+			{#if cell === 1}
+				<span class="text-5xl">🤖</span>
+			{/if}
+		</div>
+	{/each}
+</div>
